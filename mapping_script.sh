4deac749393cf4f96415509982d387b607b2f76f
Alos Diallo <alos31@gmail.com>
2022-07-13 12:32:27 -0400

    updated
    changing queue to medium

--- mapping_script.sh
+++ mapping_script.sh
@@ -13,8 +13,9 @@ do
 sample=$(echo "$R1" | cut -f 1 -d '_')
 
 #run bowtie2 on all files
-#@1,0,bowtie2,,sbatch -p short -n 1 -t 990:0 --mem 10G -c 25
-bowtie2 -x /n/groups/shared_databases/bowtie2_indexes/mm10 -1 "$sample"_R1.fastq -2 "$sample"_R2.fastq --no-unal -p 25 -S ~/scratch/ATACseqUnzip/bo
+#@1,0,bowtie2,,sbatch -p medium -n 1 -t 20:00:00 --mem 20G -c 20
+bowtie2 -x /n/groups/shared_databases/bowtie2_indexes/mm10 -1 "$sample"_R1.fastq -2 "$sample"_R2.fastq --no-unal -p 20 -S ~/scratch/ATACseqUnzip/bo
 wtie_mapped_results/"$sample".sam
 
 done
+
