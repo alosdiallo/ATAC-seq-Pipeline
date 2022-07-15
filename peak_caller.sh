81e0a049d8ab589ce130044d476840e7be26e5cf
Alos Diallo <alos31@gmail.com>
2022-07-15 13:26:31 -0400

    Update peak_caller.sh
    Added log to output

--- peak_caller.sh
+++ peak_caller.sh
@@ -6,6 +6,6 @@ do
 
 #create a prefix
 #@1,0,Samt,,sbatch -p short -n 1 -t 60:0 --mem 70G -c 1
-Genrich  -t "$bam" -o "$bam".narrowPeak  -j  -y  -r  -v -q .05 -a 20
+Genrich  -t "$bam" -o "$bam".narrowPeak -f "$bam".narrowPeak.log -j  -y  -r  -v -q .05 -a 20
 
 done
