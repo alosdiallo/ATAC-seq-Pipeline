5953e882ce05a3448886eb53cfc939c8e20f415e
Alos Diallo <alos31@gmail.com>
2022-07-15 10:50:34 -0400

    Update sorter.sh
    increase mem and core

--- sorter.sh
+++ sorter.sh
@@ -5,7 +5,7 @@ for bam in $PWD/*.bam
 do
 
 #create a prefix
-#@1,0,Samt,,sbatch -p short -n 1 -t 60:0 --mem 50G -c 10
+#@1,0,Samt,,sbatch -p short -n 1 -t 60:0 --mem 150G -c 15
 samtools sort -n "$bam" -o "$bam".sorted.bam
 
 done
