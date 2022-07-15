21f896b8add518633291631665077067fc2ca90f
Alos Diallo <alos31@gmail.com>
2022-07-15 11:32:56 -0400

    Update sorter.sh

--- sorter.sh
+++ sorter.sh
@@ -5,7 +5,7 @@ for bam in $PWD/*.bam
 do
 
 #create a prefix
-#@1,0,Samt,,sbatch -p short -n 1 -t 60:0 --mem 150G -c 15
-samtools sort -n "$bam" -o "$bam".sorted.bam
+#@1,0,Samt,,sbatch -p short -n 1 -t 60:0 --mem 120G -c 10
+samtools sort -@ 10 -m 10G -n "$bam" -o "$bam".sorted.bam
 
 done
